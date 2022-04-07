2544a18a95d01405805b10bd3a825c2e0017169d
recombinant <gituser@recombinant.me.uk>
2022-04-07 20:23:18 +0100

    feat: windowsx addional message crackers HANDLE_WM_MOUSEMOVE ..LBUTTONDOWN ..RBUTTONDOWN

--- windowsx/windowsx/windowsx.zig
+++ windowsx/windowsx/windowsx.zig
@@ -5,6 +5,7 @@ const win32 = struct {
     usingnamespace @import("win32").ui.windows_and_messaging;
 };
 const BOOL = win32.BOOL;
+const FALSE = win32.FALSE;
 const HWND = win32.HWND;
 const WPARAM = win32.WPARAM;
 const LPARAM = win32.LPARAM;
@@ -28,12 +29,21 @@ pub fn GetStockBrush(hbrush: win32.GET_STOCK_OBJECT_FLAGS) ?HBRUSH {
     return @as(?HBRUSH, win32.GetStockObject(hbrush));
 }
 
+// 0x0001
 // pub fn OnCreate(self: *T, hwnd: HWND, cs: *CREATESTRUCT) LRESULT
 pub fn HANDLE_WM_CREATE(hwnd: HWND, _: WPARAM, lParam: LPARAM, comptime T: type, handler: *T) LRESULT {
     const ptr: *CREATESTRUCT = @intToPtr(*CREATESTRUCT, @bitCast(usize, lParam));
     return if (handler.OnCreate(hwnd, ptr) == 0) 0 else -1;
 }
 
+// 0x0002
+// pub fn OnDestroy(self: *T, hwnd: HWND) void
+pub fn HANDLE_WM_DESTROY(hwnd: HWND, _: WPARAM, _: LPARAM, comptime T: type, handler: *T) LRESULT {
+    handler.OnDestroy(hwnd);
+    return 0;
+}
+
+// 0x0005
 // pub fn OnSize(self: *T, hwnd: HWND, state: u32, cx: i32, cy: i32) void
 pub fn HANDLE_WM_SIZE(hwnd: HWND, wParam: WPARAM, lParam: LPARAM, comptime T: type, handler: *T) LRESULT {
     const state = @truncate(u32, wParam);
@@ -43,6 +53,14 @@ pub fn HANDLE_WM_SIZE(hwnd: HWND, wParam: WPARAM, lParam: LPARAM, comptime T: ty
     return 0;
 }
 
+// 0x000f
+// pub fn OnPaint(self: *T, hwnd: HWND) void
+pub fn HANDLE_WM_PAINT(hwnd: HWND, _: WPARAM, _: LPARAM, comptime T: type, handler: *T) LRESULT {
+    handler.OnPaint(hwnd);
+    return 0;
+}
+
+// 0x0114
 // pub fn OnHScroll(self: *T, hwnd: HWND, hwndCtrl: ?HWND, code: u32, pos: i32) void
 pub fn HANDLE_WM_HSCROLL(hwnd: HWND, wParam: WPARAM, lParam: LPARAM, comptime T: type, handler: *T) LRESULT {
     const hwndCtrl = @intToPtr(?HWND, @bitCast(usize, lParam));
@@ -52,6 +70,7 @@ pub fn HANDLE_WM_HSCROLL(hwnd: HWND, wParam: WPARAM, lParam: LPARAM, comptime T:
     return 0;
 }
 
+// 0x0115
 // pub fn OnVScroll(self: *T, hwnd: HWND, hwndCtrl: ?HWND, code: u32, pos: i32) void
 pub fn HANDLE_WM_VSCROLL(hwnd: HWND, wParam: WPARAM, lParam: LPARAM, comptime T: type, handler: *T) LRESULT {
     const hwndCtrl = @intToPtr(?HWND, @bitCast(usize, lParam));
@@ -61,14 +80,32 @@ pub fn HANDLE_WM_VSCROLL(hwnd: HWND, wParam: WPARAM, lParam: LPARAM, comptime T:
     return 0;
 }
 
-// pub fn OnPaint(self: *T, hwnd: HWND) void
-pub fn HANDLE_WM_PAINT(hwnd: HWND, _: WPARAM, _: LPARAM, comptime T: type, handler: *T) LRESULT {
-    handler.OnPaint(hwnd);
+// 0x0200
+// pub fn OnMouseMove(self: *T, hwnd: HWND, x: i32, y: i32, keyFlags: u32) void
+pub fn HANDLE_WM_MOUSEMOVE(hwnd: HWND, wParam: WPARAM, lParam: LPARAM, comptime T: type, handler: *T) LRESULT {
+    const x: i32 = @truncate(i16, lParam);
+    const y: i32 = @truncate(i16, lParam >> 16);
+    const keyFlags = @truncate(u32, wParam);
+    handler.OnMouseMove(hwnd, x, y, keyFlags);
     return 0;
 }
 
-// pub fn OnDestroy(self: *T, hwnd: HWND) void
-pub fn HANDLE_WM_DESTROY(hwnd: HWND, _: WPARAM, _: LPARAM, comptime T: type, handler: *T) LRESULT {
-    handler.OnDestroy(hwnd);
+// 0x0201
+// pub fn OnLButtonDown(self: *T, hwnd: HWND, fDoubleClick: BOOL, x: i32, y: i32, keyFlags: u32) void
+pub fn HANDLE_WM_LBUTTONDOWN(hwnd: HWND, wParam: WPARAM, lParam: LPARAM, comptime T: type, handler: *T) LRESULT {
+    const x: i32 = @truncate(i16, lParam);
+    const y: i32 = @truncate(i16, lParam >> 16);
+    const keyFlags = @truncate(u32, wParam);
+    handler.OnLButtonDown(hwnd, FALSE, x, y, keyFlags);
+    return 0;
+}
+
+// 0x0204
+// pub fn OnRButtonDown(self: *T, hwnd: HWND, fDoubleClick: BOOL, x: i32, y: i32, keyFlags: u32) void
+pub fn HANDLE_WM_RBUTTONDOWN(hwnd: HWND, wParam: WPARAM, lParam: LPARAM, comptime T: type, handler: *T) LRESULT {
+    const x: i32 = @truncate(i16, lParam);
+    const y: i32 = @truncate(i16, lParam >> 16);
+    const keyFlags = @truncate(u32, wParam);
+    handler.OnRButtonDown(hwnd, FALSE, x, y, keyFlags);
     return 0;
 }
