f49deef3a3fa631473617b336f2cecd1ae941197
recombinant <gituser@recombinant.me.uk>
2022-04-08 22:28:34 +0100

    feat: added more HANDLE_WM_XXX message crackers

--- windowsx/windowsx/windowsx.zig
+++ windowsx/windowsx/windowsx.zig
@@ -1,10 +1,13 @@
 // Message Crackers as per the original windowsx.h
 const win32 = struct {
+    usingnamespace @import("win32").zig;
     usingnamespace @import("win32").foundation;
     usingnamespace @import("win32").graphics.gdi;
     usingnamespace @import("win32").ui.windows_and_messaging;
+    usingnamespace @import("win32").ui.input.keyboard_and_mouse;
 };
 const BOOL = win32.BOOL;
+const TRUE = win32.TRUE;
 const FALSE = win32.FALSE;
 const HWND = win32.HWND;
 const WPARAM = win32.WPARAM;
@@ -16,6 +19,7 @@ const HBRUSH = win32.HBRUSH;
 const HPEN = win32.HPEN;
 const HFONT = win32.HFONT;
 const CREATESTRUCT = win32.CREATESTRUCT;
+const VIRTUAL_KEY = win32.VIRTUAL_KEY;
 
 // ----------------------------------------------------------------------------
 
@@ -98,6 +102,26 @@ pub fn HANDLE_WM_PAINT(hwnd: HWND, _: WPARAM, _: LPARAM, comptime T: type, handl
     return 0;
 }
 
+// 0x0100
+// pub fn OnKey(self: *T, hwnd: HWND, vk: VIRTUAL_KEY, fDown: BOOL, cRepeat: i32, flags: u32) void
+pub fn HANDLE_WM_KEYDOWN(hwnd: HWND, wParam: WPARAM, lParam: LPARAM, comptime T: type, handler: *T) LRESULT {
+    const cRepeat: i32 = @truncate(i16, lParam);
+    const flags: u32 = @bitCast(u16, @truncate(i16, lParam >> 16));
+    const vk = @intToEnum(VIRTUAL_KEY, @truncate(u16, wParam));
+    handler.OnKey(hwnd, vk, TRUE, cRepeat, flags);
+    return 0;
+}
+
+// 0x0101
+// pub fn OnKey(self: *T, hwnd: HWND, vk: VIRTUAL_KEY, fDown: BOOL, cRepeat: i32, flags: u32) void
+pub fn HANDLE_WM_KEYUP(hwnd: HWND, wParam: WPARAM, lParam: LPARAM, comptime T: type, handler: *T) LRESULT {
+    const cRepeat: i32 = @truncate(i16, lParam);
+    const flags: u32 = @bitCast(u16, @truncate(i16, lParam >> 16));
+    const vk = @intToEnum(VIRTUAL_KEY, @truncate(u16, wParam));
+    handler.OnKey(hwnd, vk, FALSE, cRepeat, flags);
+    return 0;
+}
+
 // 0x0114
 // pub fn OnHScroll(self: *T, hwnd: HWND, hwndCtrl: ?HWND, code: u32, pos: i32) void
 pub fn HANDLE_WM_HSCROLL(hwnd: HWND, wParam: WPARAM, lParam: LPARAM, comptime T: type, handler: *T) LRESULT {
@@ -138,6 +162,26 @@ pub fn HANDLE_WM_LBUTTONDOWN(hwnd: HWND, wParam: WPARAM, lParam: LPARAM, comptim
     return 0;
 }
 
+// 0x0202
+// pub fn OnLButtonUp(self: *T, hwnd: HWND, x: i32, y: i32, keyFlags: u32) void
+pub fn HANDLE_WM_LBUTTONUP(hwnd: HWND, wParam: WPARAM, lParam: LPARAM, comptime T: type, handler: *T) LRESULT {
+    const x: i32 = @truncate(i16, lParam);
+    const y: i32 = @truncate(i16, lParam >> 16);
+    const keyFlags = @truncate(u32, wParam);
+    handler.OnLButtonUp(hwnd, x, y, keyFlags);
+    return 0;
+}
+
+// 0x0203
+// pub fn OnLButtonDown(self: *T, hwnd: HWND, fDoubleClick: BOOL, x: i32, y: i32, keyFlags: u32) void
+pub fn HANDLE_WM_LBUTTONDBLCLK(hwnd: HWND, wParam: WPARAM, lParam: LPARAM, comptime T: type, handler: *T) LRESULT {
+    const x: i32 = @truncate(i16, lParam);
+    const y: i32 = @truncate(i16, lParam >> 16);
+    const keyFlags = @truncate(u32, wParam);
+    handler.OnLButtonDown(hwnd, TRUE, x, y, keyFlags);
+    return 0;
+}
+
 // 0x0204
 // pub fn OnRButtonDown(self: *T, hwnd: HWND, fDoubleClick: BOOL, x: i32, y: i32, keyFlags: u32) void
 pub fn HANDLE_WM_RBUTTONDOWN(hwnd: HWND, wParam: WPARAM, lParam: LPARAM, comptime T: type, handler: *T) LRESULT {
@@ -145,5 +189,54 @@ pub fn HANDLE_WM_RBUTTONDOWN(hwnd: HWND, wParam: WPARAM, lParam: LPARAM, comptim
     const y: i32 = @truncate(i16, lParam >> 16);
     const keyFlags = @truncate(u32, wParam);
     handler.OnRButtonDown(hwnd, FALSE, x, y, keyFlags);
+    return 0;
+}
+
+// 0x0205
+// pub fn OnRButtonUp(self: *T, hwnd: HWND, x: i32, y: i32, keyFlags: u32) void
+pub fn HANDLE_WM_RBUTTONUP(hwnd: HWND, wParam: WPARAM, lParam: LPARAM, comptime T: type, handler: *T) LRESULT {
+    const x: i32 = @truncate(i16, lParam);
+    const y: i32 = @truncate(i16, lParam >> 16);
+    const keyFlags = @truncate(u32, wParam);
+    handler.OnRButtonUp(hwnd, x, y, keyFlags);
+    return 0;
+}
+
+// 0x0206
+// pub fn OnRButtonDown(self: *T, hwnd: HWND, fDoubleClick: BOOL, x: i32, y: i32, keyFlags: u32) void
+pub fn HANDLE_WM_RBUTTONDBLCLK(hwnd: HWND, wParam: WPARAM, lParam: LPARAM, comptime T: type, handler: *T) LRESULT {
+    const x: i32 = @truncate(i16, lParam);
+    const y: i32 = @truncate(i16, lParam >> 16);
+    const keyFlags = @truncate(u32, wParam);
+    handler.OnRButtonDown(hwnd, TRUE, x, y, keyFlags);
+    return 0;
+}
+// 0x0207
+// pub fn OnMButtonDown(self: *T, hwnd: HWND, fDoubleClick: BOOL, x: i32, y: i32, keyFlags: u32) void
+pub fn HANDLE_WM_MBUTTONDOWN(hwnd: HWND, wParam: WPARAM, lParam: LPARAM, comptime T: type, handler: *T) LRESULT {
+    const x: i32 = @truncate(i16, lParam);
+    const y: i32 = @truncate(i16, lParam >> 16);
+    const keyFlags = @truncate(u32, wParam);
+    handler.OnMButtonDown(hwnd, FALSE, x, y, keyFlags);
+    return 0;
+}
+
+// 0x0208
+// pub fn OnMButtonUp(self: *T, hwnd: HWND, x: i32, y: i32, keyFlags: u32) void
+pub fn HANDLE_WM_MBUTTONUP(hwnd: HWND, wParam: WPARAM, lParam: LPARAM, comptime T: type, handler: *T) LRESULT {
+    const x: i32 = @truncate(i16, lParam);
+    const y: i32 = @truncate(i16, lParam >> 16);
+    const keyFlags = @truncate(u32, wParam);
+    handler.OnMButtonUp(hwnd, x, y, keyFlags);
+    return 0;
+}
+
+// 0x0209
+// pub fn OnMButtonDown(self: *T, hwnd: HWND, fDoubleClick: BOOL, x: i32, y: i32, keyFlags: u32) void
+pub fn HANDLE_WM_MBUTTONDBLCLK(hwnd: HWND, wParam: WPARAM, lParam: LPARAM, comptime T: type, handler: *T) LRESULT {
+    const x: i32 = @truncate(i16, lParam);
+    const y: i32 = @truncate(i16, lParam >> 16);
+    const keyFlags = @truncate(u32, wParam);
+    handler.OnMButtonDown(hwnd, TRUE, x, y, keyFlags);
     return 0;
 }
