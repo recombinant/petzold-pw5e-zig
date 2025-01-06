c6e87ad34c622b86be1c5b7f6bc045334f68e26c
recombinant <gituser@recombinant.me.uk>
2025-01-06 21:33:17 +0000

    06-SysMets3 update to Zig 0.14 and contemporaneous zigwin32

--- Chapter 04 An Exercise in Text Output/06-SysMets3/SysMets3.zig
+++ Chapter 04 An Exercise in Text Output/06-SysMets3/SysMets3.zig
@@ -20,13 +20,13 @@ const buffer_sizes = @import("sysmets").buffer_sizes;
 const num_lines = @import("sysmets").num_lines;
 
 const win32 = struct {
-    usingnamespace @import("win32").zig;
-    usingnamespace @import("win32").system.library_loader;
-    usingnamespace @import("win32").foundation;
-    usingnamespace @import("win32").system.system_services;
-    usingnamespace @import("win32").ui.windows_and_messaging;
-    usingnamespace @import("win32").graphics.gdi;
-    usingnamespace @import("win32").ui.controls;
+    usingnamespace @import("zigwin32").zig;
+    usingnamespace @import("zigwin32").system.library_loader;
+    usingnamespace @import("zigwin32").foundation;
+    usingnamespace @import("zigwin32").system.system_services;
+    usingnamespace @import("zigwin32").ui.windows_and_messaging;
+    usingnamespace @import("zigwin32").graphics.gdi;
+    usingnamespace @import("zigwin32").ui.controls;
 };
 const BOOL = win32.BOOL;
 const FALSE = win32.FALSE;
@@ -41,19 +41,6 @@ const HCURSOR = win32.HCURSOR;
 const LPARAM = win32.LPARAM;
 const WPARAM = win32.WPARAM;
 const LRESULT = win32.LRESULT;
-const CREATESTRUCT = win32.CREATESTRUCT;
-const SCROLLINFO = win32.SCROLLINFO;
-const SCROLLINFO_MASK = win32.SCROLLINFO_MASK;
-const SIF_POS = win32.SIF_POS;
-const CW_USEDEFAULT = win32.CW_USEDEFAULT;
-const SB_VERT = win32.SB_VERT;
-const SB_HORZ = win32.SB_HORZ;
-const TA_TOP = @enumToInt(win32.TA_TOP);
-const TA_LEFT = @enumToInt(win32.TA_LEFT);
-const TA_RIGHT = @enumToInt(win32.TA_RIGHT);
-const WS_OVERLAPPEDWINDOW = @enumToInt(win32.WS_OVERLAPPEDWINDOW);
-const WS_VSCROLL = @enumToInt(win32.WS_VSCROLL);
-const WS_HSCROLL = @enumToInt(win32.WS_HSCROLL);
 const WM_CREATE = win32.WM_CREATE;
 const WM_SIZE = win32.WM_SIZE;
 const WM_HSCROLL = win32.WM_HSCROLL;
@@ -61,7 +48,7 @@ const WM_VSCROLL = win32.WM_VSCROLL;
 const WM_PAINT = win32.WM_PAINT;
 const WM_DESTROY = win32.WM_DESTROY;
 
-const windowsx = @import("windowsx").windowsx;
+const windowsx = @import("windowsx");
 const GetStockBrush = windowsx.GetStockBrush;
 const HANDLE_WM_CREATE = windowsx.HANDLE_WM_CREATE;
 const HANDLE_WM_SIZE = windowsx.HANDLE_WM_SIZE;
@@ -81,13 +68,13 @@ pub export fn wWinMain(
     const app_name = L("SysMets3");
     const wndclassex = win32.WNDCLASSEX{
         .cbSize = @sizeOf(win32.WNDCLASSEX),
-        .style = win32.WNDCLASS_STYLES.initFlags(.{ .HREDRAW = 1, .VREDRAW = 1 }),
+        .style = win32.WNDCLASS_STYLES{ .HREDRAW = 1, .VREDRAW = 1 },
         .lpfnWndProc = WndProc,
         .cbClsExtra = 0,
         .cbWndExtra = 0,
         .hInstance = hInstance,
-        .hIcon = @ptrCast(HICON, win32.LoadImage(null, win32.IDI_APPLICATION, win32.IMAGE_ICON, 0, 0, win32.IMAGE_FLAGS.initFlags(.{ .SHARED = 1, .DEFAULTSIZE = 1 }))),
-        .hCursor = @ptrCast(HCURSOR, win32.LoadImage(null, win32.IDC_ARROW, win32.IMAGE_CURSOR, 0, 0, win32.IMAGE_FLAGS.initFlags(.{ .SHARED = 1, .DEFAULTSIZE = 1 }))),
+        .hIcon = @ptrCast(win32.LoadImage(null, win32.IDI_APPLICATION, win32.IMAGE_ICON, 0, 0, win32.IMAGE_FLAGS{ .SHARED = 1, .DEFAULTSIZE = 1 })),
+        .hCursor = @ptrCast(win32.LoadImage(null, win32.IDC_ARROW, win32.IMAGE_CURSOR, 0, 0, win32.IMAGE_FLAGS{ .SHARED = 1, .DEFAULTSIZE = 1 })),
         .hbrBackground = GetStockBrush(win32.WHITE_BRUSH),
         .lpszMenuName = null,
         .lpszClassName = app_name,
@@ -96,27 +83,28 @@ pub export fn wWinMain(
 
     const atom: u16 = win32.RegisterClassEx(&wndclassex);
     if (0 == atom) {
-        std.debug.print("failed RegisterClassEx()", .{});
+        std.log.err("failed RegisterClassEx()", .{});
         return 0; // premature exit
     }
 
-    // If a memory align panic occurs then the CreateWindowExW() Zig declaration
-    // needs to have align(1) added to the lpClassName parameter.
-    //   lpClassName: ?[*:0]align(1) const u16,
-    //                      ^^^^^^^^
+    const ws_tiled_window: u32 = @bitCast(win32.WS_TILEDWINDOW);
+    const ws_vscroll: u32 = @bitCast(win32.WS_VSCROLL);
+    const ws_hscroll: u32 = @bitCast(win32.WS_HSCROLL);
+    const dwStyle: win32.WINDOW_STYLE = @bitCast(ws_tiled_window | ws_vscroll | ws_hscroll);
+
+    // If a memory align panic occurs with CreateWindowExW() lpClassName then look at:
     // https://github.com/marlersoft/zigwin32gen/issues/9
-    const lpClassName = @intToPtr([*:0]align(1) const u16, atom);
 
     const hwnd = win32.CreateWindowEx(
         // https://docs.microsoft.com/en-us/windows/win32/winmsg/extended-window-styles
-        win32.WINDOW_EX_STYLE.initFlags(.{}),
-        lpClassName,
+        win32.WINDOW_EX_STYLE{},
+        @ptrFromInt(atom),
         L("Get System Metrics No. 3"),
-        @intToEnum(win32.WINDOW_STYLE, WS_OVERLAPPEDWINDOW | WS_VSCROLL | WS_HSCROLL),
-        CW_USEDEFAULT, // initial x position
-        CW_USEDEFAULT, // initial y position
-        CW_USEDEFAULT, // initial x size
-        CW_USEDEFAULT, // initial y size
+        comptime dwStyle,
+        win32.CW_USEDEFAULT, // initial x position
+        win32.CW_USEDEFAULT, // initial y position
+        win32.CW_USEDEFAULT, // initial x size
+        win32.CW_USEDEFAULT, // initial y size
         null, // parent window handle
         null, // window menu handle
         win32.GetModuleHandle(null),
@@ -124,13 +112,13 @@ pub export fn wWinMain(
     );
 
     if (null == hwnd) {
-        std.debug.print("failed CreateWindowEx(), error {}", .{win32.GetLastError()});
+        std.log.err("failed CreateWindowEx(), error {}", .{win32.GetLastError()});
         return 0; // premature exit
     }
 
-    _ = win32.ShowWindow(hwnd, @intToEnum(win32.SHOW_WINDOW_CMD, nCmdShow));
+    _ = win32.ShowWindow(hwnd, @bitCast(nCmdShow));
     if (0 == win32.UpdateWindow(hwnd)) {
-        std.debug.print("failed UpdateWindow()", .{});
+        std.log.err("failed UpdateWindow()", .{});
         return 0; // premature exit
     }
 
@@ -141,7 +129,7 @@ pub export fn wWinMain(
         if (-1 == ret) {
             // handle the error and/or exit
             // for error call GetLastError();
-            std.debug.print("failed message loop, error {}", .{win32.GetLastError()});
+            std.log.err("failed message loop, error {}", .{win32.GetLastError()});
             return 0;
         } else {
             _ = win32.TranslateMessage(&msg);
@@ -151,7 +139,7 @@ pub export fn wWinMain(
     }
 
     // Normal exit
-    return @bitCast(c_int, @truncate(c_uint, msg.wParam)); // WM_QUIT
+    return @bitCast(@as(c_uint, @truncate(msg.wParam))); // WM_QUIT
 }
 
 const Handler = struct {
@@ -162,12 +150,10 @@ const Handler = struct {
     client_height: i32 = undefined,
     max_column_width: i32 = undefined,
 
-    pub fn OnCreate(self: *Handler, hwnd: HWND, _: *CREATESTRUCT) LRESULT {
+    pub fn OnCreate(self: *Handler, hwnd: HWND, _: *win32.CREATESTRUCT) LRESULT {
         {
             const hdc = win32.GetDC(hwnd);
-            defer {
-                _ = win32.ReleaseDC(hwnd, hdc);
-            }
+            defer _ = win32.ReleaseDC(hwnd, hdc);
 
             var tm: win32.TEXTMETRIC = undefined;
             _ = win32.GetTextMetrics(hdc, &tm);
@@ -190,45 +176,45 @@ const Handler = struct {
 
         // Set vertical scroll bar range and page size
         {
-            var si = SCROLLINFO{
-                .cbSize = @sizeOf(SCROLLINFO),
-                .fMask = SCROLLINFO_MASK.initFlags(.{ .RANGE = 1, .PAGE = 1 }),
+            var si = win32.SCROLLINFO{
+                .cbSize = @sizeOf(win32.SCROLLINFO),
+                .fMask = win32.SCROLLINFO_MASK{ .RANGE = 1, .PAGE = 1 },
                 .nMin = 0,
                 .nMax = num_lines - 1,
-                .nPage = @intCast(u32, @divTrunc(self.client_height, self.char_height)),
+                .nPage = @intCast(@divTrunc(self.client_height, self.char_height)),
                 .nPos = undefined,
                 .nTrackPos = undefined,
             };
-            _ = win32.SetScrollInfo(hwnd, SB_VERT, &si, TRUE);
+            _ = win32.SetScrollInfo(hwnd, win32.SB_VERT, &si, TRUE);
         }
 
         // Set horizontal scroll bar range and page size
         {
-            var si = SCROLLINFO{
-                .cbSize = @sizeOf(SCROLLINFO),
-                .fMask = SCROLLINFO_MASK.initFlags(.{ .RANGE = 1, .PAGE = 1 }),
+            var si = win32.SCROLLINFO{
+                .cbSize = @sizeOf(win32.SCROLLINFO),
+                .fMask = win32.SCROLLINFO_MASK{ .RANGE = 1, .PAGE = 1 },
                 .nMin = 0,
                 .nMax = 2 + @divTrunc(self.max_column_width, self.char_width),
-                .nPage = @intCast(u32, @divTrunc(self.client_width, self.char_width)),
+                .nPage = @intCast(@divTrunc(self.client_width, self.char_width)),
                 .nPos = undefined,
                 .nTrackPos = undefined,
             };
-            _ = win32.SetScrollInfo(hwnd, SB_HORZ, &si, TRUE);
+            _ = win32.SetScrollInfo(hwnd, win32.SB_HORZ, &si, TRUE);
         }
     }
 
     pub fn OnVScroll(self: *Handler, hwnd: HWND, _: ?HWND, code: u16, _: i16) void {
         // Get all the vertical scroll bar information
-        var si = SCROLLINFO{
-            .cbSize = @sizeOf(SCROLLINFO),
-            .fMask = SCROLLINFO_MASK.initFlags(.{ .ALL = 1 }),
+        var si = win32.SCROLLINFO{
+            .cbSize = @sizeOf(win32.SCROLLINFO),
+            .fMask = win32.SIF_ALL,
             .nMin = undefined,
             .nMax = undefined,
             .nPage = undefined,
             .nPos = undefined,
             .nTrackPos = undefined,
         };
-        _ = win32.GetScrollInfo(hwnd, SB_VERT, &si);
+        _ = win32.GetScrollInfo(hwnd, win32.SB_VERT, &si);
 
         // Save the position for comparison later on
 
@@ -239,8 +225,8 @@ const Handler = struct {
             win32.SB_BOTTOM => si.nPos = si.nMax,
             win32.SB_LINEUP => si.nPos -= 1,
             win32.SB_LINEDOWN => si.nPos += 1,
-            win32.SB_PAGEUP => si.nPos -= @intCast(i32, si.nPage),
-            win32.SB_PAGEDOWN => si.nPos += @intCast(i32, si.nPage),
+            win32.SB_PAGEUP => si.nPos -= @as(i32, @intCast(si.nPage)),
+            win32.SB_PAGEDOWN => si.nPos += @as(i32, @intCast(si.nPage)),
             win32.SB_THUMBTRACK => si.nPos = si.nTrackPos,
             else => {},
         }
@@ -248,9 +234,9 @@ const Handler = struct {
         // Set the position and then retrieve it.  Due to adjustments
         //   by Windows it may not be the same as the value set.
 
-        si.fMask = SIF_POS;
-        _ = win32.SetScrollInfo(hwnd, SB_VERT, &si, TRUE);
-        _ = win32.GetScrollInfo(hwnd, SB_VERT, &si);
+        si.fMask = win32.SIF_POS;
+        _ = win32.SetScrollInfo(hwnd, win32.SB_VERT, &si, TRUE);
+        _ = win32.GetScrollInfo(hwnd, win32.SB_VERT, &si);
 
         // If the position has changed, scroll the window and update it
 
@@ -262,9 +248,9 @@ const Handler = struct {
 
     pub fn OnHScroll(self: *Handler, hwnd: HWND, _: ?HWND, code: u16, _: i16) void {
         // Get all the horizontal scroll bar information
-        var si = SCROLLINFO{
-            .cbSize = @sizeOf(SCROLLINFO),
-            .fMask = SCROLLINFO_MASK.initFlags(.{ .ALL = 1 }),
+        var si = win32.SCROLLINFO{
+            .cbSize = @sizeOf(win32.SCROLLINFO),
+            .fMask = win32.SIF_ALL,
             .nMin = undefined,
             .nMax = undefined,
             .nPage = undefined,
@@ -274,14 +260,14 @@ const Handler = struct {
 
         // Save the position for comparison later on
 
-        _ = win32.GetScrollInfo(hwnd, SB_HORZ, &si);
-        var iHorzPos = si.nPos;
+        _ = win32.GetScrollInfo(hwnd, win32.SB_HORZ, &si);
+        const iHorzPos = si.nPos;
 
         switch (code) {
             win32.SB_LINELEFT => si.nPos -= 1,
             win32.SB_LINERIGHT => si.nPos += 1,
-            win32.SB_PAGELEFT => si.nPos -= @intCast(i32, si.nPage),
-            win32.SB_PAGERIGHT => si.nPos += @intCast(i32, si.nPage),
+            win32.SB_PAGELEFT => si.nPos -= @as(i32, @intCast(si.nPage)),
+            win32.SB_PAGERIGHT => si.nPos += @as(i32, @intCast(si.nPage)),
             win32.SB_THUMBPOSITION => si.nPos = si.nTrackPos,
             else => {},
         }
@@ -289,9 +275,9 @@ const Handler = struct {
         // Set the position and then retrieve it.  Due to adjustments
         //   by Windows it may not be the same as the value set.
 
-        si.fMask = SIF_POS;
-        _ = win32.SetScrollInfo(hwnd, SB_HORZ, &si, TRUE);
-        _ = win32.GetScrollInfo(hwnd, SB_HORZ, &si);
+        si.fMask = win32.SIF_POS;
+        _ = win32.SetScrollInfo(hwnd, win32.SB_HORZ, &si, TRUE);
+        _ = win32.GetScrollInfo(hwnd, win32.SB_HORZ, &si);
 
         // If the position has changed, scroll the window
 
@@ -303,13 +289,11 @@ const Handler = struct {
     pub fn OnPaint(self: *Handler, hwnd: HWND) void {
         var ps: win32.PAINTSTRUCT = undefined;
         const hdc: ?HDC = win32.BeginPaint(hwnd, &ps);
-        defer {
-            _ = win32.EndPaint(hwnd, &ps);
-        }
+        defer _ = win32.EndPaint(hwnd, &ps);
 
-        var si = SCROLLINFO{
-            .cbSize = @sizeOf(SCROLLINFO),
-            .fMask = SCROLLINFO_MASK.initFlags(.{ .POS = 1 }),
+        var si = win32.SCROLLINFO{
+            .cbSize = @sizeOf(win32.SCROLLINFO),
+            .fMask = win32.SCROLLINFO_MASK{ .POS = 1 },
             .nMin = undefined,
             .nMax = undefined,
             .nPage = undefined,
@@ -318,23 +302,26 @@ const Handler = struct {
         };
 
         // Get vertical scroll bar position
-        _ = win32.GetScrollInfo(hwnd, SB_VERT, &si);
+        _ = win32.GetScrollInfo(hwnd, win32.SB_VERT, &si);
         const iVertPos = si.nPos;
 
         // Get horizontal scroll bar position
-        _ = win32.GetScrollInfo(hwnd, SB_HORZ, &si);
+        _ = win32.GetScrollInfo(hwnd, win32.SB_HORZ, &si);
         const iHorzPos = si.nPos;
 
         // Find painting limits
-        const iPaintBeg = @maximum(0, iVertPos + @divTrunc(ps.rcPaint.top, self.char_height));
-        const iPaintEnd = @minimum(num_lines - 1, iVertPos + @divTrunc(ps.rcPaint.bottom, self.char_height));
+        const iPaintBeg = @max(0, iVertPos + @divTrunc(ps.rcPaint.top, self.char_height));
+        const iPaintEnd = @min(num_lines - 1, iVertPos + @divTrunc(ps.rcPaint.bottom, self.char_height));
 
-        const flagsL = @intToEnum(win32.TEXT_ALIGN_OPTIONS, TA_LEFT | TA_TOP);
-        const flagsR = @intToEnum(win32.TEXT_ALIGN_OPTIONS, TA_RIGHT | TA_TOP);
+        const TA_LEFT = @intFromEnum(win32.TA_LEFT);
+        const TA_RIGHT = @intFromEnum(win32.TA_RIGHT);
+        const TA_TOP = @intFromEnum(win32.TA_TOP);
+        const flagsL: win32.TEXT_ALIGN_OPTIONS = @enumFromInt(TA_LEFT | TA_TOP);
+        const flagsR: win32.TEXT_ALIGN_OPTIONS = @enumFromInt(TA_RIGHT | TA_TOP);
 
         var i = iPaintBeg;
         while (i <= iPaintEnd) : (i += 1) {
-            const metric = sysmetrics[@intCast(usize, i)];
+            const metric = sysmetrics[@intCast(i)];
 
             const x = self.char_width * (1 - iHorzPos);
             const y = self.char_height * (i - iVertPos);
@@ -342,27 +329,33 @@ const Handler = struct {
             // Convert text to Windows UTF16
 
             var label: [buffer_sizes.label]u16 = [_]u16{0} ** buffer_sizes.label;
-            var label_len: i32 = @intCast(i32, std.unicode.utf8ToUtf16Le(label[0..], metric.label) catch unreachable);
+            const label_len: i32 = @intCast(std.unicode.utf8ToUtf16Le(label[0..], metric.label) catch unreachable);
 
             var description: [buffer_sizes.description]u16 = [_]u16{0} ** buffer_sizes.description;
-            var description_len = @intCast(i32, std.unicode.utf8ToUtf16Le(description[0..], metric.description) catch unreachable);
+            const description_len: i32 = @intCast(std.unicode.utf8ToUtf16Le(description[0..], metric.description) catch unreachable);
 
-            var buffer2: [6]u8 = [_]u8{0} ** 6;
-            const slice2 = std.fmt.bufPrint(buffer2[0..], "{d:5}", .{win32.GetSystemMetrics(metric.index)}) catch unreachable;
-
+            // Do the merry dance to remove the + sign when printing positive signed integers on Zig 0.12.0
+            const slice2 = blk: {
+                var buffer2: [6]u8 = [_]u8{0} ** 6;
+                const value = win32.GetSystemMetrics(metric.index);
+                if (value >= 0)
+                    break :blk std.fmt.bufPrint(buffer2[0..], "{d:5}", .{@as(u32, @bitCast(value))}) catch unreachable
+                else
+                    break :blk std.fmt.bufPrint(buffer2[0..], "{d:5}", .{value}) catch unreachable;
+            };
             var index: [6]u16 = [_]u16{0} ** 6;
-            var index_len = @intCast(i32, std.unicode.utf8ToUtf16Le(index[0..], slice2) catch unreachable);
+            const index_len: i32 = @intCast(std.unicode.utf8ToUtf16Le(index[0..], slice2) catch unreachable);
 
             // Output text
 
             _ = win32.SetTextAlign(hdc, flagsL);
 
-            _ = win32.TextOut(hdc, x, y, &label, label_len);
-            _ = win32.TextOut(hdc, x + 22 * self.caps_width, y, &description, description_len);
+            _ = win32.TextOut(hdc, x, y, @ptrCast(&label), label_len);
+            _ = win32.TextOut(hdc, x + 22 * self.caps_width, y, @ptrCast(&description), description_len);
 
             _ = win32.SetTextAlign(hdc, flagsR);
 
-            _ = win32.TextOut(hdc, x + 22 * self.caps_width + 40 * self.char_width, y, &index, index_len);
+            _ = win32.TextOut(hdc, x + 22 * self.caps_width + 40 * self.char_width, y, @ptrCast(&index), index_len);
         }
     }
 
@@ -371,21 +364,23 @@ const Handler = struct {
     }
 };
 
-var handler = Handler{};
-
 fn WndProc(
     hwnd: HWND,
     message: u32,
     wParam: WPARAM,
     lParam: LPARAM,
 ) callconv(WINAPI) LRESULT {
+    const state = struct {
+        var handler = Handler{};
+    };
+
     return switch (message) {
-        WM_CREATE => HANDLE_WM_CREATE(hwnd, wParam, lParam, Handler, &handler),
-        WM_SIZE => HANDLE_WM_SIZE(hwnd, wParam, lParam, Handler, &handler),
-        WM_HSCROLL => HANDLE_WM_HSCROLL(hwnd, wParam, lParam, Handler, &handler),
-        WM_VSCROLL => HANDLE_WM_VSCROLL(hwnd, wParam, lParam, Handler, &handler),
-        WM_PAINT => HANDLE_WM_PAINT(hwnd, wParam, lParam, Handler, &handler),
-        WM_DESTROY => HANDLE_WM_DESTROY(hwnd, wParam, lParam, Handler, &handler),
+        WM_CREATE => HANDLE_WM_CREATE(hwnd, wParam, lParam, Handler, &state.handler),
+        WM_SIZE => HANDLE_WM_SIZE(hwnd, wParam, lParam, Handler, &state.handler),
+        WM_HSCROLL => HANDLE_WM_HSCROLL(hwnd, wParam, lParam, Handler, &state.handler),
+        WM_VSCROLL => HANDLE_WM_VSCROLL(hwnd, wParam, lParam, Handler, &state.handler),
+        WM_PAINT => HANDLE_WM_PAINT(hwnd, wParam, lParam, Handler, &state.handler),
+        WM_DESTROY => HANDLE_WM_DESTROY(hwnd, wParam, lParam, Handler, &state.handler),
         else => win32.DefWindowProc(hwnd, message, wParam, lParam),
     };
 }
