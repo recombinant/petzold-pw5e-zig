80214a1b9d1a42f363cd12b424a8be39bd797fcf
recombinant <gituser@recombinant.me.uk>
2025-01-06 20:48:22 +0000

    refactor: HelloWin.zig use win32.WS_TILEDWINDOW

--- Chapter 03 Windows and Messages/03-HelloWin/HelloWin.zig
+++ Chapter 03 Windows and Messages/03-HelloWin/HelloWin.zig
@@ -84,10 +84,7 @@ pub export fn wWinMain(
         WINDOW_EX_STYLE{},
         @ptrFromInt(atom),
         L("The Hello Program"),
-        win32.WINDOW_STYLE{
-            // .TILEDWINDOW aka .OVERLAPPEDWINDOW equivalent
-            .SYSMENU = 1,
-        },
+        win32.WS_TILEDWINDOW,
         win32.CW_USEDEFAULT, // initial x position
         win32.CW_USEDEFAULT, // initial y position
         win32.CW_USEDEFAULT, // initial x size
