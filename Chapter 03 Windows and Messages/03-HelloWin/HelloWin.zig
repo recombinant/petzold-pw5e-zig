39b696193a0fbd81c391f7ba29f9d49b52f59d12
recombinant <gituser@recombinant.me.uk>
2025-01-06 16:57:52 +0000

    03-HelloWin update to Zig 0.14 and contemporaneous zigwin32

--- Chapter 03 Windows and Messages/03-HelloWin/HelloWin.zig
+++ Chapter 03 Windows and Messages/03-HelloWin/HelloWin.zig
@@ -16,18 +16,18 @@ const std = @import("std");
 const WINAPI = std.os.windows.WINAPI;
 
 const win32 = struct {
-    usingnamespace @import("win32").zig;
-    usingnamespace @import("win32").system.library_loader;
-    usingnamespace @import("win32").foundation;
-    usingnamespace @import("win32").system.system_services;
-    usingnamespace @import("win32").ui.windows_and_messaging;
-    usingnamespace @import("win32").graphics.gdi;
-    usingnamespace @import("win32").media.audio;
+    usingnamespace @import("zigwin32").zig;
+    usingnamespace @import("zigwin32").system.library_loader;
+    usingnamespace @import("zigwin32").foundation;
+    usingnamespace @import("zigwin32").system.system_services;
+    usingnamespace @import("zigwin32").ui.windows_and_messaging;
+    usingnamespace @import("zigwin32").graphics.gdi;
+    usingnamespace @import("zigwin32").media.audio;
 };
 const BOOL = win32.BOOL;
 const L = win32.L;
 const HINSTANCE = win32.HINSTANCE;
-const CW_USEDEFAULT = win32.CW_USEDEFAULT;
+// const CW_USEDEFAULT = win32.CW_USEDEFAULT;
 const MSG = win32.MSG;
 const HWND = win32.HWND;
 const HDC = win32.HDC;
@@ -37,14 +37,14 @@ const WNDCLASSEX = win32.WNDCLASSEX;
 const WNDCLASS_STYLES = win32.WNDCLASS_STYLES;
 const WINDOW_EX_STYLE = win32.WINDOW_EX_STYLE;
 const WINDOW_STYLE = win32.WINDOW_STYLE;
-const IDI_APPLICATION = win32.IDI_APPLICATION;
-const IDC_ARROW = win32.IDC_ARROW;
-const IMAGE_ICON = win32.IMAGE_ICON;
-const IMAGE_CURSOR = win32.IMAGE_CURSOR;
+// const IDI_APPLICATION = win32.IDI_APPLICATION;
+// const IDC_ARROW = win32.IDC_ARROW;
+// const IMAGE_ICON = win32.IMAGE_ICON;
+// const IMAGE_CURSOR = win32.IMAGE_CURSOR;
 const IMAGE_FLAGS = win32.IMAGE_FLAGS;
-const WHITE_BRUSH = win32.WHITE_BRUSH;
+// const WHITE_BRUSH = win32.WHITE_BRUSH;
 
-const windowsx = @import("windowsx").windowsx;
+const windowsx = @import("windowsx");
 
 pub export fn wWinMain(
     hInstance: HINSTANCE,
@@ -57,14 +57,14 @@ pub export fn wWinMain(
     const app_name = L("HelloWin");
     const wndclassex = WNDCLASSEX{
         .cbSize = @sizeOf(WNDCLASSEX),
-        .style = WNDCLASS_STYLES.initFlags(.{ .HREDRAW = 1, .VREDRAW = 1 }),
+        .style = WNDCLASS_STYLES{ .HREDRAW = 1, .VREDRAW = 1 },
         .lpfnWndProc = WndProc,
         .cbClsExtra = 0,
         .cbWndExtra = 0,
         .hInstance = hInstance,
-        .hIcon = @ptrCast(HICON, win32.LoadImage(null, IDI_APPLICATION, IMAGE_ICON, 0, 0, IMAGE_FLAGS.initFlags(.{ .SHARED = 1, .DEFAULTSIZE = 1 }))),
-        .hCursor = @ptrCast(HCURSOR, win32.LoadImage(null, IDC_ARROW, IMAGE_CURSOR, 0, 0, IMAGE_FLAGS.initFlags(.{ .SHARED = 1, .DEFAULTSIZE = 1 }))),
-        .hbrBackground = windowsx.GetStockBrush(WHITE_BRUSH),
+        .hIcon = @ptrCast(win32.LoadImage(null, win32.IDI_APPLICATION, win32.IMAGE_ICON, 0, 0, IMAGE_FLAGS{ .SHARED = 1, .DEFAULTSIZE = 1 })),
+        .hCursor = @ptrCast(win32.LoadImage(null, win32.IDC_ARROW, win32.IMAGE_CURSOR, 0, 0, IMAGE_FLAGS{ .SHARED = 1, .DEFAULTSIZE = 1 })),
+        .hbrBackground = windowsx.GetStockBrush(win32.WHITE_BRUSH),
         .lpszMenuName = null,
         .lpszClassName = app_name,
         .hIconSm = null,
@@ -72,29 +72,26 @@ pub export fn wWinMain(
 
     const atom: u16 = win32.RegisterClassEx(&wndclassex);
     if (0 == atom) {
-        std.debug.print("failed RegisterClassEx()", .{});
+        std.log.err("failed RegisterClassEx()", .{});
         return 0; // premature exit
     }
 
-    // If a memory align panic occurs then the CreateWindowExW() Zig declaration
-    // needs to have align(1) added to the lpClassName parameter.
-    //   lpClassName: ?[*:0]align(1) const u16,
-    //                      ^^^^^^^^
+    // If a memory align panic occurs with CreateWindowExW() then look at:
     // https://github.com/marlersoft/zigwin32gen/issues/9
-    const lpClassName = @intToPtr([*:0]align(1) const u16, atom);
 
     const hwnd = win32.CreateWindowEx(
         // https://docs.microsoft.com/en-us/windows/win32/winmsg/extended-window-styles
-        WINDOW_EX_STYLE.initFlags(.{}),
-        lpClassName,
+        WINDOW_EX_STYLE{},
+        @ptrFromInt(atom),
         L("The Hello Program"),
-        win32.WINDOW_STYLE.initFlags(.{
-            .TILEDWINDOW = 1, // .OVERLAPPEDWINDOW equivalent
-        }),
-        CW_USEDEFAULT, // initial x position
-        CW_USEDEFAULT, // initial y position
-        CW_USEDEFAULT, // initial x size
-        CW_USEDEFAULT, // initial y size
+        win32.WINDOW_STYLE{
+            // .TILEDWINDOW aka .OVERLAPPEDWINDOW equivalent
+            .SYSMENU = 1,
+        },
+        win32.CW_USEDEFAULT, // initial x position
+        win32.CW_USEDEFAULT, // initial y position
+        win32.CW_USEDEFAULT, // initial x size
+        win32.CW_USEDEFAULT, // initial y size
         null, // parent window handle
         null, // window menu handle
         win32.GetModuleHandle(null),
@@ -102,13 +99,13 @@ pub export fn wWinMain(
     );
 
     if (null == hwnd) {
-        std.debug.print("failed CreateWindowEx(), error {}", .{win32.GetLastError()});
+        std.log.err("failed CreateWindowEx(), error {}", .{win32.GetLastError()});
         return 0; // premature exit
     }
 
-    _ = win32.ShowWindow(hwnd, @intToEnum(win32.SHOW_WINDOW_CMD, nCmdShow));
+    _ = win32.ShowWindow(hwnd, @bitCast(nCmdShow));
     if (0 == win32.UpdateWindow(hwnd)) {
-        std.debug.print("failed UpdateWindow()", .{});
+        std.log.err("failed UpdateWindow()", .{});
         return 0; // premature exit
     }
 
@@ -119,7 +116,7 @@ pub export fn wWinMain(
         if (-1 == ret) {
             // handle the error and/or exit
             // for error call GetLastError();
-            std.debug.print("failed message loop, error {}", .{win32.GetLastError()});
+            std.log.err("failed message loop, error {}", .{win32.GetLastError()});
             return 0;
         } else {
             _ = win32.TranslateMessage(&msg);
@@ -128,26 +125,18 @@ pub export fn wWinMain(
         ret = win32.GetMessage(&msg, null, 0, 0);
     }
 
-    std.debug.print("normal exit", .{});
+    std.log.info("normal exit", .{});
 
-    return @bitCast(c_int, @truncate(c_uint, msg.wParam)); // WM_QUIT
+    return @bitCast(@as(c_uint, @truncate(msg.wParam))); // WM_QUIT
 }
 
-fn WndProc(
-    hwnd: HWND,
-    message: u32,
-    wParam: win32.WPARAM,
-    lParam: win32.LPARAM,
-) callconv(WINAPI) win32.LRESULT {
+fn WndProc(hwnd: HWND, message: u32, wParam: win32.WPARAM, lParam: win32.LPARAM) callconv(WINAPI) win32.LRESULT {
     switch (message) {
         win32.WM_CREATE => {
             const data = @embedFile("HelloWin.wav");
-            const pointer = @intToPtr([*:0]const u16, @ptrToInt(data));
-            _ = win32.PlaySound(pointer, null, win32.SND_MEMORY | win32.SND_ASYNC);
-            // _ = win32.PlaySound(L("HelloWin.wav"), null, win32.SND_FILENAME | win32.SND_ASYNC);
+            _ = win32.PlaySound(@ptrCast(@alignCast(data)), null, win32.SND_FLAGS{ .MEMORY = 1, .ASYNC = 1 });
             return 0; // message processed
         },
-
         win32.WM_PAINT => {
             var ps: win32.PAINTSTRUCT = undefined;
             const hdc: ?HDC = win32.BeginPaint(hwnd, &ps);
@@ -160,13 +149,12 @@ fn WndProc(
                 L("Hello, Windows 98!"),
                 -1,
                 &rect,
-                win32.DRAW_TEXT_FORMAT.initFlags(.{ .SINGLELINE = 1, .CENTER = 1, .VCENTER = 1 }),
+                win32.DRAW_TEXT_FORMAT{ .SINGLELINE = 1, .CENTER = 1, .VCENTER = 1 },
             );
 
             _ = win32.EndPaint(hwnd, &ps);
             return 0; // message processed
         },
-
         win32.WM_DESTROY => {
             win32.PostQuitMessage(0);
             return 0; // message processed
--- Chapter 03 Windows and Messages/03-HelloWin/build.zig
+++ Chapter 03 Windows and Messages/03-HelloWin/build.zig
@@ -1,52 +1,34 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const exe_mod = b.createModule(.{
+        .root_source_file = b.path("HelloWin.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const exe = b.addExecutable(.{
+        .name = "HelloWin",
+        .root_module = exe_mod,
+    });
 
-    const exe = b.addExecutable("01-HelloWin", "HelloWin.zig");
-    exe.single_threaded = true;
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
-    exe.install();
+    exe.root_module.addImport(
+        "zigwin32",
+        b.dependency("zigwin32", .{}).module("zigwin32"),
+    );
+    exe.root_module.addImport(
+        "windowsx",
+        b.dependency("windowsx", .{}).module("windowsx"),
+    );
+    b.installArtifact(exe);
 
-    const pkg1 = std.build.Pkg{
-        .name = "win32",
-        .path = .{ .path = "../../zigwin32/win32.zig" },
-    };
-    const pkg2 = std.build.Pkg{
-        .name = "windowsx",
-        .path = .{ .path = "../../windowsx/windowsx.zig" },
-        .dependencies = &[_]std.build.Pkg{pkg1},
-    };
-
-    exe.addPackage(pkg1);
-    exe.addPackage(pkg2);
-
-    const run_cmd = exe.run();
+    const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-
-    const exe_tests = b.addTest("HelloWin.zig");
-    exe_tests.setTarget(target);
-    exe_tests.setBuildMode(mode);
-    exe_tests.addPackage(.{
-        .name = "win32",
-        .path = .{ .path = "../../zigwin32/win32.zig" },
-    });
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&exe_tests.step);
 }
