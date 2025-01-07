80cd42087f522fa170721bdbb66a3b670f50aa40
recombinant <gituser@recombinant.me.uk>
2025-01-07 21:08:58 +0000

    13-RandRect update to Zig 0.14 and contemporaneous zigwin32

--- Chapter 05 Basic Drawing/13-RandRect/RandRect.zig
+++ Chapter 05 Basic Drawing/13-RandRect/RandRect.zig
@@ -12,17 +12,16 @@
 pub const UNICODE = true;
 
 const std = @import("std");
-const RndGen = std.rand.DefaultPrng;
 
 const WINAPI = std.os.windows.WINAPI;
 
 const win32 = struct {
-    usingnamespace @import("win32").zig;
-    usingnamespace @import("win32").system.library_loader;
-    usingnamespace @import("win32").foundation;
-    usingnamespace @import("win32").system.system_services;
-    usingnamespace @import("win32").ui.windows_and_messaging;
-    usingnamespace @import("win32").graphics.gdi;
+    usingnamespace @import("zigwin32").zig;
+    usingnamespace @import("zigwin32").system.library_loader;
+    usingnamespace @import("zigwin32").foundation;
+    usingnamespace @import("zigwin32").system.system_services;
+    usingnamespace @import("zigwin32").ui.windows_and_messaging;
+    usingnamespace @import("zigwin32").graphics.gdi;
 };
 const BOOL = win32.BOOL;
 const FALSE = win32.FALSE;
@@ -30,20 +29,13 @@ const L = win32.L;
 const HINSTANCE = win32.HINSTANCE;
 const MSG = win32.MSG;
 const HWND = win32.HWND;
-const HDC = win32.HDC;
 const LPARAM = win32.LPARAM;
 const WPARAM = win32.WPARAM;
 const LRESULT = win32.LRESULT;
-const CW_USEDEFAULT = win32.CW_USEDEFAULT;
-const WS_OVERLAPPEDWINDOW = win32.WS_OVERLAPPEDWINDOW;
-const WM_SIZE = win32.WM_SIZE;
-const WM_DESTROY = win32.WM_DESTROY;
 const WM_QUIT = win32.WM_QUIT;
 
-const windowsx = @import("windowsx").windowsx;
+const windowsx = @import("windowsx");
 const GetStockBrush = windowsx.GetStockBrush;
-const HANDLE_WM_SIZE = windowsx.HANDLE_WM_SIZE;
-const HANDLE_WM_DESTROY = windowsx.HANDLE_WM_DESTROY;
 
 pub export fn wWinMain(
     hInstance: HINSTANCE,
@@ -54,13 +46,13 @@ pub export fn wWinMain(
     const app_name = L("RandRect");
     const wndclassex = win32.WNDCLASSEX{
         .cbSize = @sizeOf(win32.WNDCLASSEX),
-        .style = win32.WNDCLASS_STYLES.initFlags(.{ .HREDRAW = 1, .VREDRAW = 1 }),
+        .style = win32.WNDCLASS_STYLES{ .HREDRAW = 1, .VREDRAW = 1 },
         .lpfnWndProc = WndProc,
         .cbClsExtra = 0,
         .cbWndExtra = 0,
         .hInstance = hInstance,
-        .hIcon = @ptrCast(win32.HICON, win32.LoadImage(null, win32.IDI_APPLICATION, win32.IMAGE_ICON, 0, 0, win32.IMAGE_FLAGS.initFlags(.{ .SHARED = 1, .DEFAULTSIZE = 1 }))),
-        .hCursor = @ptrCast(win32.HCURSOR, win32.LoadImage(null, win32.IDC_ARROW, win32.IMAGE_CURSOR, 0, 0, win32.IMAGE_FLAGS.initFlags(.{ .SHARED = 1, .DEFAULTSIZE = 1 }))),
+        .hIcon = @ptrCast(win32.LoadImage(null, win32.IDI_APPLICATION, win32.IMAGE_ICON, 0, 0, win32.IMAGE_FLAGS{ .SHARED = 1, .DEFAULTSIZE = 1 })),
+        .hCursor = @ptrCast(win32.LoadImage(null, win32.IDC_ARROW, win32.IMAGE_CURSOR, 0, 0, win32.IMAGE_FLAGS{ .SHARED = 1, .DEFAULTSIZE = 1 })),
         .hbrBackground = GetStockBrush(win32.WHITE_BRUSH),
         .lpszMenuName = null,
         .lpszClassName = app_name,
@@ -69,27 +61,23 @@ pub export fn wWinMain(
 
     const atom: u16 = win32.RegisterClassEx(&wndclassex);
     if (0 == atom) {
-        std.debug.print("failed RegisterClassEx()", .{});
+        std.log.err("failed RegisterClassEx()", .{});
         return 0; // premature exit
     }
 
-    // If a memory align panic occurs then the CreateWindowExW() Zig declaration
-    // needs to have align(1) added to the lpClassName parameter.
-    //   lpClassName: ?[*:0]align(1) const u16,
-    //                      ^^^^^^^^
+    // If a memory align panic occurs with CreateWindowExW() lpClassName then look at:
     // https://github.com/marlersoft/zigwin32gen/issues/9
-    const lpClassName = @intToPtr([*:0]align(1) const u16, atom);
 
     const hwnd = win32.CreateWindowEx(
         // https://docs.microsoft.com/en-us/windows/win32/winmsg/extended-window-styles
-        win32.WINDOW_EX_STYLE.initFlags(.{}),
-        lpClassName,
+        win32.WINDOW_EX_STYLE{},
+        @ptrFromInt(atom),
         L("Random Rectangles"),
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, // initial x position
-        CW_USEDEFAULT, // initial y position
-        CW_USEDEFAULT, // initial x size
-        CW_USEDEFAULT, // initial y size
+        win32.WS_OVERLAPPEDWINDOW,
+        win32.CW_USEDEFAULT, // initial x position
+        win32.CW_USEDEFAULT, // initial y position
+        win32.CW_USEDEFAULT, // initial x size
+        win32.CW_USEDEFAULT, // initial y size
         null, // parent window handle
         null, // window menu handle
         win32.GetModuleHandle(null),
@@ -97,58 +85,57 @@ pub export fn wWinMain(
     );
 
     if (null == hwnd) {
-        std.debug.print("failed CreateWindowEx(), error {}", .{win32.GetLastError()});
+        std.log.err("failed CreateWindowEx(), error {}", .{win32.GetLastError()});
         return 0; // premature exit
     }
 
-    _ = win32.ShowWindow(hwnd, @intToEnum(win32.SHOW_WINDOW_CMD, nCmdShow));
+    var prng = std.Random.DefaultPrng.init(blk: {
+        var seed: u64 = undefined;
+        std.posix.getrandom(std.mem.asBytes(&seed)) catch unreachable;
+        break :blk seed;
+    });
+    handler.random = prng.random();
+
+    _ = win32.ShowWindow(hwnd, @bitCast(nCmdShow));
     if (0 == win32.UpdateWindow(hwnd)) {
-        std.debug.print("failed UpdateWindow()", .{});
+        std.log.err("failed UpdateWindow()", .{});
         return 0; // premature exit
     }
 
     var msg: MSG = undefined;
-    var ret: BOOL = win32.GetMessage(&msg, null, 0, 0); // three states: -1, 0 or non-zero
 
-    while (0 != ret) {
-        if (-1 == ret) {
-            // handle the error and/or exit
-            // for error call GetLastError();
-            std.debug.print("failed message loop, error {}", .{win32.GetLastError()});
-            return 0;
-        } else if (win32.PeekMessage(&msg, null, 0, 0, win32.PM_REMOVE) != FALSE) {
+    while (true) {
+        if (win32.PeekMessage(&msg, null, 0, 0, win32.PM_REMOVE) != FALSE) {
             if (msg.message == WM_QUIT)
                 break;
-
             _ = win32.TranslateMessage(&msg);
             _ = win32.DispatchMessage(&msg);
         } else {
             DrawRectangle(hwnd);
         }
-        ret = win32.GetMessage(&msg, null, 0, 0);
     }
 
     // Normal exit
-    return @bitCast(c_int, @truncate(c_uint, msg.wParam)); // WM_QUIT
+    return @bitCast(@as(c_uint, @truncate(msg.wParam))); // WM_QUIT
 }
 
 fn DrawRectangle(hwnd: ?HWND) void {
     if (handler.cxClient == 0 or handler.cyClient == 0)
         return;
 
-    var rnd = handler.rnd.random();
+    const random = handler.random;
 
     var rect: win32.RECT = undefined;
-    const xLeft = rnd.intRangeLessThan(i32, 0, handler.cxClient);
-    const yTop = rnd.intRangeLessThan(i32, 0, handler.cyClient);
-    const xRight = rnd.intRangeLessThan(i32, 0, handler.cxClient);
-    const yBottom = rnd.intRangeLessThan(i32, 0, handler.cyClient);
+    const xLeft = random.intRangeLessThan(i32, 0, handler.cxClient);
+    const yTop = random.intRangeLessThan(i32, 0, handler.cyClient);
+    const xRight = random.intRangeLessThan(i32, 0, handler.cxClient);
+    const yBottom = random.intRangeLessThan(i32, 0, handler.cyClient);
 
     _ = win32.SetRect(&rect, xLeft, yTop, xRight, yBottom);
 
-    const r = rnd.intRangeLessThan(u32, 0, 256);
-    const g = rnd.intRangeLessThan(u32, 0, 256);
-    const b = rnd.intRangeLessThan(u32, 0, 256);
+    const r = random.intRangeLessThan(u32, 0, 256);
+    const g = random.intRangeLessThan(u32, 0, 256);
+    const b = random.intRangeLessThan(u32, 0, 256);
     const color = r | g << 8 | b << 16;
 
     const hBrush = win32.CreateSolidBrush(color);
@@ -162,7 +149,7 @@ fn DrawRectangle(hwnd: ?HWND) void {
 const Handler = struct {
     cxClient: i32 = undefined,
     cyClient: i32 = undefined,
-    rnd: std.rand.DefaultPrng = RndGen.init(0),
+    random: std.Random = undefined,
 
     pub fn OnSize(self: *Handler, _: HWND, _: u32, cx: i16, cy: i16) void {
         self.cxClient = cx;
@@ -173,6 +160,11 @@ const Handler = struct {
         win32.PostQuitMessage(0);
     }
 };
+
+const WM_SIZE = win32.WM_SIZE;
+const WM_DESTROY = win32.WM_DESTROY;
+const HANDLE_WM_SIZE = windowsx.HANDLE_WM_SIZE;
+const HANDLE_WM_DESTROY = windowsx.HANDLE_WM_DESTROY;
 
 var handler = Handler{};
 
