f073dfa65dbdfb8cd941c99d842e1f685148f07e
recombinant <gituser@recombinant.me.uk>
2025-03-06 10:38:35 +0000

    update to Zig 0.14.0

--- Chapter 05 Basic Drawing/13-RandRect/build.zig
+++ Chapter 05 Basic Drawing/13-RandRect/build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const win32 = b.dependency("win32", .{}).module("win32");
+    const win32 = b.dependency("zigwin32", .{}).module("win32");
     const windowsx = b.dependency("windowsx", .{}).module("windowsx");
 
     const exe_mod = b.createModule(.{
